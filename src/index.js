bd9d375bc3ca2c9a480d9f4362f9d13853fa53db
lky1001 <lky1001@gmail.com>
2018-08-29 00:00:03 +0900

    bootstrap css header

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'mobx-react'
 
+import 'bootstrap/dist/css/bootstrap.css'
+
 import eosAgent from './EosAgent'
 import eosioStore from './stores/eosioStore'
 import accountStore from './stores/accountStore'
