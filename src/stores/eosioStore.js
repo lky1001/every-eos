aa78d3be5a0abb0abccfc5950f1f509bce544b3e
kai.1001 <kai.1001@kakaocorp.com>
2018-08-31 14:39:57 +0900

    create api struct

--- src/stores/eosioStore.js
+++ src/stores/eosioStore.js
@@ -2,6 +2,7 @@ import { decorate, observable, action } from 'mobx'
 import eosAgent from '../EosAgent'
 
 class EosioStore {
+  everyeoswalletAccount = null
   info = null
 
   getInfo = async () => {
@@ -10,6 +11,48 @@ class EosioStore {
 
       this.info = eosInfo
     } catch (e) {}
+  }
+
+  buyToken = async (contract, data) => {
+    //memo : {"type":"buy-limit","symbol": "IQ","market":"IQ_EOS","price":0.00101,"qty":100,"amount":0.101}
+    const cb = tr => {
+      const options = { authorization: [`${data.accountName}@${data.authority}`] }
+
+      tr.transfer(
+        {
+          from: data.accountName,
+          to: this.everyeoswalletAccount,
+          quantity: `${Number(data.quantity)
+            .toFixed(data.precision)
+            .toString()} ${data.symbol}`,
+          memo: data.memo
+        },
+        options
+      )
+    }
+
+    return await eosAgent.createTransactionWithContract(contract, cb)
+  }
+
+  sellToken = async (contract, data) => {
+    //memo : {"type":"sell-limit","symbol": "IQ","market":"IQ_EOS","price":0.00255,"qty":44.89,"amount":0.1144}
+    const cb = tr => {
+      const options = { authorization: [`${data.accountName}@${data.authority}`] }
+
+      tr.transfer(
+        {
+          from: data.accountName,
+          to: this.everyeoswalletAccount,
+          quantity: `${Number(data.quantity)
+            .toFixed(data.precision)
+            .toString()} ${data.symbol}`,
+          memo: data.memo
+        },
+        options
+      )
+    }
+
+    return await eosAgent.createTransactionWithContract(contract, cb)
   }
 }
 
--- src/stores/marketStore.js
+++ src/stores/marketStore.js
@@ -7,6 +7,24 @@ class MarketStore {
   getTokenList = async () => {
     const tokens = await axios('http://localhost:3000/tokens')
   }
+
+  /**
+   * symbol : IQ
+   * market : IQ_EOS
+   */
+  getTokenInfo = async (symbol, market) => {
+    // last price
+    // 24H change
+    // 24H High
+    // 24H Low
+    // 24H Volume
+  }
+
+  getCurrentOrder = async accountName => {}
+
+  getOrderHistory = async accountName => {}
+
+  cancelOrder = async (accountName, id) => {}
 }
 
 decorate(MarketStore, {
