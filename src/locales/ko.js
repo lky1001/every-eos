db915556eed6ff7bf9e19ba14b098ad9d4e8e881
kai.1001 <kai.1001@kakaocorp.com>
2018-09-05 09:21:18 +0900

    translate korean

--- src/locales/ko.js
+++ src/locales/ko.js
@@ -1,8 +1,8 @@
 export default {
   Name: '이름',
-  'Last Price': '종가',
-  'Today Change': '변동',
-  '24h High': '고가',
-  '24h Low': '저가',
-  '24h Volume': '총액'
+  'Last Price': '현재가',
+  'Today Change': '전일대비',
+  '24h High': '고가(24H)',
+  '24h Low': '저가(24H)',
+  '24h Volume': '거래량(24H)'
 }
