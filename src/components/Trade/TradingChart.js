bc8e53cef99081a81c73a56a846c45d18fcd0389
chaser <epochchaser@gmail.com>
2018-09-07 23:23:48 +0900

    interactive chart with OHLC, EMA, MACD

--- src/components/Trade/TradingChart.js
+++ src/components/Trade/TradingChart.js
@@ -3,29 +3,181 @@ import { inject, observer } from 'mobx-react'
 import { compose } from 'recompose'
 import PropTypes from 'prop-types'
 import { format } from 'd3-format'
+import { timeFormat } from 'd3-time-format'
+
 import { ChartCanvas, Chart } from 'react-stockcharts'
-import { BarSeries, CandlestickSeries } from 'react-stockcharts/lib/series'
+import { BarSeries, CandlestickSeries, LineSeries, MACDSeries } from 'react-stockcharts/lib/series'
 import { XAxis, YAxis } from 'react-stockcharts/lib/axes'
+import {
+  CrossHairCursor,
+  EdgeIndicator,
+  CurrentCoordinate,
+  MouseCoordinateX,
+  MouseCoordinateY
+} from 'react-stockcharts/lib/coordinates'
+
 import { discontinuousTimeScaleProvider } from 'react-stockcharts/lib/scale'
+import { OHLCTooltip, MovingAverageTooltip, MACDTooltip } from 'react-stockcharts/lib/tooltip'
+import { ema, macd } from 'react-stockcharts/lib/indicator'
 import { fitWidth } from 'react-stockcharts/lib/helper'
-import { last } from 'react-stockcharts/lib/utils'
+import { TrendLine, DrawingObjectSelector } from 'react-stockcharts/lib/interactive'
+import { last, toObject } from 'react-stockcharts/lib/utils'
+
+import { saveInteractiveNodes, getInteractiveNodes } from '../../utils/stockChartInteractiveUtil'
+
+const macdAppearance = {
+  stroke: {
+    macd: '#FF0000',
+    signal: '#00F300'
+  },
+  fill: {
+    divergence: '#4682B4'
+  }
+}
 
 class TradingChart extends Component {
+  constructor(props) {
+    super(props)
+    this.onKeyPress = this.onKeyPress.bind(this)
+    this.onDrawCompleteChart1 = this.onDrawCompleteChart1.bind(this)
+    this.onDrawCompleteChart3 = this.onDrawCompleteChart3.bind(this)
+    this.handleSelection = this.handleSelection.bind(this)
+
+    this.saveInteractiveNodes = saveInteractiveNodes.bind(this)
+    this.getInteractiveNodes = getInteractiveNodes.bind(this)
+
+    this.saveCanvasNode = this.saveCanvasNode.bind(this)
+
+    this.state = {
+      enableTrendLine: false,
+      trends_1: [
+        { start: [1606, 56], end: [1711, 53], appearance: { stroke: 'green' }, type: 'XLINE' }
+      ],
+      trends_3: []
+    }
+  }
+
+  saveCanvasNode(node) {
+    this.canvasNode = node
+  }
+  componentDidMount() {
+    document.addEventListener('keyup', this.onKeyPress)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.onKeyPress)
+  }
+
+  handleSelection(interactives) {
+    const state = toObject(interactives, each => {
+      return [`trends_${each.chartId}`, each.objects]
+    })
+    this.setState(state)
+  }
+  onDrawCompleteChart1(trends_1) {
+    // this gets called on
+    // 1. draw complete of trendline
+    // 2. drag complete of trendline
+    console.log(trends_1)
+    this.setState({
+      enableTrendLine: false,
+      trends_1
+    })
+  }
+  onDrawCompleteChart3(trends_3) {
+    // this gets called on
+    // 1. draw complete of trendline
+    // 2. drag complete of trendline
+    console.log(trends_3)
+    this.setState({
+      enableTrendLine: false,
+      trends_3
+    })
+  }
+  onKeyPress(e) {
+    const keyCode = e.which
+    console.log(keyCode)
+    switch (keyCode) {
+    case 46: {
+      // DEL
+
+      const trends_1 = this.state.trends_1.filter(each => !each.selected)
+      const trends_3 = this.state.trends_3.filter(each => !each.selected)
+
+      this.canvasNode.cancelDrag()
+      this.setState({
+        trends_1,
+        trends_3
+      })
+      break
+    }
+    case 27: {
+      // ESC
+      this.node_1.terminate()
+      this.node_3.terminate()
+      this.canvasNode.cancelDrag()
+      this.setState({
+        enableTrendLine: false
+      })
+      break
+    }
+    case 68: // D - Draw trendline
+    case 69: {
+      // E - Enable trendline
+      this.setState({
+        enableTrendLine: true
+      })
+      break
+    }
+    default:
+      break
+    }
+  }
+
   render() {
+    const ema26 = ema()
+      .id(0)
+      .options({ windowSize: 26 })
+      .merge((d, c) => {
+        d.ema26 = c
+      })
+      .accessor(d => d.ema26)
+
+    const ema12 = ema()
+      .id(1)
+      .options({ windowSize: 12 })
+      .merge((d, c) => {
+        d.ema12 = c
+      })
+      .accessor(d => d.ema12)
+
+    const macdCalculator = macd()
+      .options({
+        fast: 12,
+        slow: 26,
+        signal: 9
+      })
+      .merge((d, c) => {
+        d.macd = c
+      })
+      .accessor(d => d.macd)
+
     const { type, chartData: initialData, width, ratio } = this.props
+
+    const calculatedData = macdCalculator(ema12(ema26(initialData)))
     const xScaleProvider = discontinuousTimeScaleProvider.inputDateAccessor(d => d.date)
-    const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(initialData)
+    const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(calculatedData)
 
     const start = xAccessor(last(data))
-    const end = xAccessor(data[Math.max(0, data.length - 100)])
+    const end = xAccessor(data[Math.max(0, data.length - 150)])
     const xExtents = [start, end]
 
     return (
       <ChartCanvas
+        ref={this.saveCanvasNode}
         height={600}
-        ratio={ratio}
         width={width}
-        margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+        ratio={ratio}
+        margin={{ left: 70, right: 70, top: 20, bottom: 30 }}
         type={type}
         seriesName="MSFT"
         data={data}
@@ -33,23 +185,116 @@ class TradingChart extends Component {
         xAccessor={xAccessor}
         displayXAccessor={displayXAccessor}
         xExtents={xExtents}>
-        <Chart id={1} height={400} yExtents={d => [d.high, d.low]}>
+        <Chart
+          id={1}
+          height={400}
+          yExtents={[d => [d.high, d.low], ema26.accessor(), ema12.accessor()]}
+          padding={{ top: 10, bottom: 20 }}>
+          <XAxis axisAt="bottom" orient="bottom" showTicks={false} outerTickSize={0} />
           <YAxis axisAt="right" orient="right" ticks={5} />
-          <XAxis axisAt="bottom" orient="bottom" showTicks={false} />
+          <MouseCoordinateY at="right" orient="right" displayFormat={format('.2f')} />
+
           <CandlestickSeries />
+          <LineSeries yAccessor={ema26.accessor()} stroke={ema26.stroke()} />
+          <LineSeries yAccessor={ema12.accessor()} stroke={ema12.stroke()} />
+
+          <CurrentCoordinate yAccessor={ema26.accessor()} fill={ema26.stroke()} />
+          <CurrentCoordinate yAccessor={ema12.accessor()} fill={ema12.stroke()} />
+
+          <EdgeIndicator
+            itemType="last"
+            orient="right"
+            edgeAt="right"
+            yAccessor={d => d.close}
+            fill={d => (d.close > d.open ? '#6BA583' : '#FF0000')}
+          />
+
+          <OHLCTooltip origin={[-40, 0]} />
+
+          <MovingAverageTooltip
+            onClick={e => console.log(e)}
+            origin={[-38, 15]}
+            options={[
+              {
+                yAccessor: ema26.accessor(),
+                type: ema26.type(),
+                stroke: ema26.stroke(),
+                windowSize: ema26.options().windowSize
+              },
+              {
+                yAccessor: ema12.accessor(),
+                type: ema12.type(),
+                stroke: ema12.stroke(),
+                windowSize: ema12.options().windowSize
+              }
+            ]}
+          />
+          <TrendLine
+            ref={this.saveInteractiveNodes('Trendline', 1)}
+            enabled={this.state.enableTrendLine}
+            type="RAY"
+            snap={false}
+            snapTo={d => [d.high, d.low]}
+            onStart={() => console.log('START')}
+            onComplete={this.onDrawCompleteChart1}
+            trends={this.state.trends_1}
+          />
         </Chart>
-        <Chart id={2} origin={(w, h) => [0, h - 150]} height={150} yExtents={d => d.volume}>
-          <XAxis axisAt="bottom" orient="bottom" />
+        <Chart id={2} height={150} yExtents={[d => d.volume]} origin={(w, h) => [0, h - 300]}>
           <YAxis axisAt="left" orient="left" ticks={5} tickFormat={format('.2s')} />
-          <BarSeries yAccessor={d => d.volume} fill={d => (d.close > d.open ? '#6BA583' : 'red')} />
+
+          <MouseCoordinateY at="left" orient="left" displayFormat={format('.4s')} />
+
+          <BarSeries
+            yAccessor={d => d.volume}
+            fill={d => (d.close > d.open ? '#6BA583' : '#FF0000')}
+          />
         </Chart>
+        <Chart
+          id={3}
+          height={150}
+          yExtents={macdCalculator.accessor()}
+          origin={(w, h) => [0, h - 150]}
+          padding={{ top: 10, bottom: 10 }}>
+          <XAxis axisAt="bottom" orient="bottom" />
+          <YAxis axisAt="right" orient="right" ticks={2} />
+
+          <MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat('%Y-%m-%d')} />
+          <MouseCoordinateY at="right" orient="right" displayFormat={format('.2f')} />
+          <TrendLine
+            ref={this.saveInteractiveNodes('Trendline', 3)}
+            enabled={this.state.enableTrendLine}
+            type="RAY"
+            snap={false}
+            snapTo={d => [d.high, d.low]}
+            onStart={() => console.log('START')}
+            onComplete={this.onDrawCompleteChart3}
+            trends={this.state.trends_3}
+          />
+          <MACDSeries yAccessor={d => d.macd} {...macdAppearance} />
+          <MACDTooltip
+            origin={[-38, 15]}
+            yAccessor={d => d.macd}
+            options={macdCalculator.options()}
+            appearance={macdAppearance}
+          />
+        </Chart>
+        <CrossHairCursor />
+        <DrawingObjectSelector
+          enabled={!this.state.enableTrendLine}
+          getInteractiveNodes={this.getInteractiveNodes}
+          drawingObjectMap={{
+            Trendline: 'trends'
+          }}
+          onSelect={this.handleSelection}
+        />
       </ChartCanvas>
     )
   }
 }
 
 TradingChart.propTypes = {
-  chartData: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
   width: PropTypes.number.isRequired,
   ratio: PropTypes.number.isRequired,
   type: PropTypes.oneOf(['svg', 'hybrid']).isRequired
@@ -58,6 +303,7 @@ TradingChart.propTypes = {
 TradingChart.defaultProps = {
   type: 'svg'
 }
+
 TradingChart = fitWidth(TradingChart)
 
 export default compose(
