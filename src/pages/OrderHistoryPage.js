85c01cbf4c4fcd9c6b9754af959074737d912e21
chaser <epochchaser@gmail.com>
2018-09-19 00:53:47 +0900

    orders page

--- src/pages/OrderHistoryPage.js
+++ src/pages/OrderHistoryPage.js
@@ -1,9 +1,32 @@
 import React, { Component, Fragment } from 'react'
+import { Grid, Row, Col } from 'react-bootstrap'
+import OrderHistory from '../components/Trade/OrderHistory'
+import { inject, observer } from 'mobx-react'
+import { compose } from 'recompose'
 
-class Order extends Component {
+class OrderHistoryPage extends Component {
   render() {
-    return <Fragment />
+    const { accountStore, tradeStore } = this.props
+
+    return (
+      <Fragment>
+        <Grid>
+          <Row>
+            <Col>
+              <OrderHistory
+                accountStore={accountStore}
+                tradeStore={tradeStore}
+                ordersHistoryList={tradeStore.ordersHistoryList}
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </Fragment>
+    )
   }
 }
 
-export default Order
+export default compose(
+  inject('tradeStore', 'accountStore'),
+  observer
+)(OrderHistoryPage)
