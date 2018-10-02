554fbc57b7a67323fa4586413d6842ad447dd4ee
kai.1001 <kai.1001@kakaocorp.com>
2018-10-02 13:48:12 +0900

    order result message

--- src/components/Trade/Order.js
+++ src/components/Trade/Order.js
@@ -7,11 +7,7 @@ import { withAlert } from 'react-alert'
 import { FormattedMessage } from 'react-intl'
 import ColorsConstant from '../Colors/ColorsConstant'
 import { RightAlignCol } from '../Common/Common'
-import {
-  EOS_TOKEN,
-  SCATTER_ERROR_LOCKED,
-  SCATTER_ERROR_REJECT_TRANSACTION_BY_USER
-} from '../../constants/Values'
+import { EOS_TOKEN, SCATTER_ERROR_LOCKED, SCATTER_ERROR_REJECT_TRANSACTION_BY_USER } from '../../constants/Values'
 
 import styled from 'styled-components'
 
@@ -34,8 +30,7 @@ const OrderColPanel = styled(Col)`
 
 const PrimaryOrderColPanel = styled(OrderColPanel)`
   text-align: left;
-  color: ${props =>
-    props.buy ? ColorsConstant.Thick_green : props.sell && ColorsConstant.Thick_red};
+  color: ${props => (props.buy ? ColorsConstant.Thick_green : props.sell && ColorsConstant.Thick_red)};
 `
 
 const OrderInput = styled(Input)`
@@ -149,9 +144,7 @@ class Order extends Component {
     let eosBalance = await accountStore.getTokenBalance(EOS_TOKEN.symbol, EOS_TOKEN.contract)
     eosBalance = parseFloat(eosBalance)
 
-    const eosAmount = parseFloat(this.state.buyPrice * this.state.buyQty).toFixed(
-      EOS_TOKEN.precision
-    )
+    const eosAmount = parseFloat(this.state.buyPrice * this.state.buyQty).toFixed(EOS_TOKEN.precision)
 
     if (eosAmount < 0.1) {
       this.props.alert.show('Order is must greater then or equal to 0.1000 EOS.')
@@ -189,6 +182,7 @@ class Order extends Component {
 
       if (result) {
         tradeStore.getPollingOrderByTxId(result.transaction_id)
+        this.props.alert.show('Success(' + result.transaction_id + ')')
       }
     } catch (e) {
       this.handleError(e)
@@ -239,6 +233,7 @@ class Order extends Component {
       const result = await eosioStore.buyToken(EOS_TOKEN.contract, data)
 
       if (result) {
+        this.props.alert.show('Success(' + result.transaction_id + ')')
         alert(JSON.stringify(result))
       }
     } catch (e) {
@@ -293,6 +288,7 @@ class Order extends Component {
       const result = await eosioStore.buyToken(token.contract, data)
 
       if (result) {
+        this.props.alert.show('Success(' + result.transaction_id + ')')
         tradeStore.getPollingOrderByTxId(result.transaction_id)
       }
     } catch (e) {
@@ -338,6 +334,7 @@ class Order extends Component {
       const result = await eosioStore.buyToken(token.contract, data)
 
       if (result) {
+        this.props.alert.show('Success(' + result.transaction_id + ')')
         alert(JSON.stringify(result))
       }
     } catch (e) {
@@ -347,9 +344,9 @@ class Order extends Component {
 
   handleError = e => {
     if (e.code === SCATTER_ERROR_LOCKED) {
-      // todo
+      this.props.alert.show('Scatter is locked.')
     } else if (e.code === SCATTER_ERROR_REJECT_TRANSACTION_BY_USER) {
-      // todo
+      this.props.alert.show('Cancelled.')
     }
   }
 
@@ -383,12 +380,7 @@ class Order extends Component {
                 </OrderColPanel>
                 <Col sm="9">
                   <InputGroup>
-                    <OrderInput
-                      type="number"
-                      value={this.state.buyPrice}
-                      onChange={this.handleChange('buyPrice')}
-                      step="1"
-                    />
+                    <OrderInput type="number" value={this.state.buyPrice} onChange={this.handleChange('buyPrice')} step="1" />
                     <InputGroupAddon addonType="append">EOS</InputGroupAddon>
                   </InputGroup>
                 </Col>
@@ -399,13 +391,7 @@ class Order extends Component {
                 </OrderColPanel>
                 <Col sm="9">
                   <InputGroup style={{ width: '100%' }}>
-                    <OrderInput
-                      placeholder="Amount"
-                      type="number"
-                      step="1"
-                      onChange={this.handleChange('buyQty')}
-                      value={this.state.buyQty}
-                    />
+                    <OrderInput placeholder="Amount" type="number" step="1" onChange={this.handleChange('buyQty')} value={this.state.buyQty} />
                     <InputGroupAddon addonType="append">{token.symbol}</InputGroupAddon>
                   </InputGroup>
                 </Col>
@@ -436,12 +422,7 @@ class Order extends Component {
                 </OrderColPanel>
                 <Col sm="9">
                   <InputGroup>
-                    <OrderInput
-                      type="number"
-                      onChange={this.handleChange('sellPrice')}
-                      value={this.state.sellPrice}
-                      step="1"
-                    />
+                    <OrderInput type="number" onChange={this.handleChange('sellPrice')} value={this.state.sellPrice} step="1" />
                     <InputGroupAddon addonType="append">EOS</InputGroupAddon>
                   </InputGroup>
                 </Col>
@@ -452,13 +433,7 @@ class Order extends Component {
                 </OrderColPanel>
                 <Col sm="9">
                   <InputGroup style={{ width: '100%' }}>
-                    <OrderInput
-                      placeholder="Amount"
-                      type="number"
-                      step="1"
-                      onChange={this.handleChange('sellQty')}
-                      value={this.state.sellQty}
-                    />
+                    <OrderInput placeholder="Amount" type="number" step="1" onChange={this.handleChange('sellQty')} value={this.state.sellQty} />
                     <InputGroupAddon addonType="append">{token.symbol}</InputGroupAddon>
                   </InputGroup>
                 </Col>
